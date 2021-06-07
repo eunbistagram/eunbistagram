1a37bd3be0f87ab803a6b22e098266a892f053bb
eunbistagram <eunbistagram@gmail.com>
2021-06-06 23:48:56 -0600

    added stories + tweeting system + timecheck. test tmr and fix env variables theyre not permanent for some reason

--- eunbistagram.py
+++ eunbistagram.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
     
     
     
-#eunbistagram.py
+#eunbistagram.py -- feed posts -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 EUNBI_ID = '47636361181'
 UUID = api.generate_uuid(return_hex=False, seed=None);
 
@@ -131,10 +131,12 @@ def getFeed(userID):
 	user_feed = api.user_feed(userID)
 	feed_items = user_feed.get('items')
 	return feed_items
+
 	
 def findPost(feed, index):
 	post = feed[index]
 	return post
+		
 
 
 
@@ -175,18 +177,18 @@ def saveMedia(post):
 			identifyMediaType(media)
 			if isPhoto:
 				upload = media['image_versions2']['candidates'][0]['url']
-				urllib.request.urlretrieve(upload, 'album-%s-P.jpg' % i)
+				urllib.request.urlretrieve(upload, 'posts/album-%s.jpg' % i)
 			else:
 				containsVideo = True
 				upload = media['video_versions'][0]['url']
-				urllib.request.urlretrieve(upload, 'album-%s-V.mp4' % i)
+				urllib.request.urlretrieve(upload, 'posts/album-%s.mp4' % i)
 	elif isPhoto:
 		upload = post['image_versions2']['candidates'][0]['url']
-		urllib.request.urlretrieve(upload, 'img.jpg')
+		urllib.request.urlretrieve(upload, 'posts/post.jpg')
 		
 	elif isVideo:
 		upload = post['video_versions'][0]['url']
-		urllib.request.urlretrieve(upload, 'video.mp4')
+		urllib.request.urlretrieve(upload, 'posts/post.mp4')
 		
 	else:
 		print('Unidentified media type\n')
@@ -197,25 +199,110 @@ def saveMedia(post):
 	
 def getMediaFile():
 	media_files = []
-	for f in os.listdir('.'):
+	for f in os.listdir('posts'):
 		if os.path.isfile(f) and 'album-' in f:
 			media_files.append(f)
-		elif os.path.isfile(f) and 'img' in f:
+		elif os.path.isfile(f) and '.jpg' in f:
 			media_files = f
-		elif os.path.isfile(f) and 'video' in f:
+		elif os.path.isfile(f) and '.mp4' in f:
 			media_files = f
 		else:
 			continue
 			
 	return media_files
+
+
+def authTwitter(): 
+	consumer_key = os.environ.get('CONSUMER_KEY')
+	consumer_secret = os.environ.get('CONSUMER_SECRET')
+	access_token = os.environ.get('ACCESS_TOKEN')
+	access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+
+	api = tweepy.API(auth)
+
+	return api
+
+def tweetPost(media_files, api):
+
+	vid_ids = []
+	pic_ids = []
+
+	if containsVideo:
+		pic_ids.append([pic for pic in media_files if '.jpg' in pic])
+		vid_ids.append([vid for vid in media_files if '.mp4' in vid])
+		
+		if len(vid_ids) == 0:
+			if len(pic_ids) <= 4:
+				api.update_status(media_ids=pic_ids, status='test')
+			elif len(pic_ids) <= 8:
+				post1 = api.update_status(media_ids=pic_ids[0:4], status='test')
+				post2 = api.update_status(media_ids=pic_ids[4::], status='test2', in_reply_to_status_id=post1.id)
+			elif len(pic_ids) <= 10:
+				post1 = api.update_status(media_ids=pic_ids[0:4], status='test')
+				post2 = api.update_status(media_ids=pic_ids[4:8], status='test2', in_reply_to_status_id=post1.id)
+				post3 = api.update_status(media_ids=pic_ids[8::], status='test3', in_reply_to_status_id=post2.id)
+			else:
+				print('Error at tweetPost and containsVideo. More than 10 pics possibly')
+				
+		elif len(pic_ids) == 0:
+			post = api.update_status(media_ids=vid_ids[0], status='test')
+			vid_ids.pop(0)
+			for vid in vid_ids:
+				post= api.update_status(media_ids=vid, in_reply_to_status_id=post.id)
+		
+		else:
+			if len(pic_ids) <= 4:
+				post = api.update_status(media_ids=pic_ids, status='test')
+				for vid in vid_ids:
+					post = api.update_status(media_ids=vid, in_reply_to_status_id=post.id)
+					
+			elif len(pic_ids) <= 8:
+				post = api.update_status(media_ids=pic_ids[0:4], status='test')
+				post = api.update_status(media_ids=pic_ids[4::], status='test2', in_reply_to_status_id=post.id)
+				for vid in vid_ids:
+					post = api.update_status(media_ids=vid, in_reply_to_status_id=post.id)
+					
+			elif len(pic_ids) <= 10:
+				post = api.update_status(media_ids=pic_ids[0:4], status='test')
+				post = api.update_status(media_ids=pic_ids[4:8], status='test2', in_reply_to_status_id=post.id)
+				post = api.update_status(media_ids=pic_ids[8::], status='test3', in_reply_to_status_id=post.id)
+				for vid in vid_ids:
+					post = api.update_status(media_ids=vid, in_reply_to_status_id=post.id)
+			else:
+				print('Error at tweetPost and containsVideo. More than 10 pics possibly.')
+			
+
+
+	elif isAlbum and not containsVideo:
+		pic_ids.append([pic for pic in media_files if '.jpg' in pic])
+		if len(pic_ids) <= 4:
+			api.update_status(media_ids=pic_ids)
+		elif len(pic_ids) <= 8:
+			post1 = api.update_status(media_ids=pic_ids[0:4], status='test')
+			post2 = api.update_status(media_ids=pic_ids[4::], status='test2', in_reply_to_status_id=post1.id)
+		elif len(pic_ids) <=10:
+			post1 = api.update_status(media_ids=pic_ids[0:4], status='test')
+			post2 = api.update_status(media_ids=pic_ids[4:8], status='test2', in_reply_to_status_id=post1.id)
+			post3 = api.update_status(media_ids=pic_ids[8::], status='test3', in_reply_to_status_id=post2.id)
+		else:
+			print('Error at tweetPost. isAlbum and not containsVideo. More than 10 images')
 	
+	elif isPhoto:
+		pic_ids.append([pic for pic in media_files if '.jpg' in pic])
+		api.update_status(media_ids=pic_ids[0], status = '[%s INSTAGRAM PHOTO]' % getTimeStamp(latest_post))
+	elif isVideo:
+		vid_ids.append([vid for vid in media_files if '.mp4' in pic])
+		api.update_status(media_ids=vid_ids[0], status = '[%s INSTAGRAM VIDEO]' % getTimeStamp(latest_post))
+		
+	else:
+		print('Problem at tweetPost')
+		
+
 
 latest_post = findPost(getFeed(EUNBI_ID), 0)
-
-saveMedia(latest_post)
-
-	
-new = True
 
 with open ('timestamps.txt', 'a+') as logfile:
 	timestamp = latest_post['taken_at']
@@ -226,47 +313,131 @@ with open ('timestamps.txt', 'a+') as logfile:
 	else:
 		new = True
 		logfile.write(str(timestamp) + '\n')
+		
 if new:
 	saveMedia(latest_post)
 	media_files = getMediaFile()
+	tweetPost(media_files, authTwitter())
 else:
 	print('No new update')
+	
+
+
+
+
+
+
+
+
+
+#eunbistagram.py --- story posts -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+story_reel = api.user_story_feed(EUNBI_ID)['reel']
+if story_reel == None:
+	print('No live stories on instagram.com/silver_rain.__')
 	sys.exit();
+
+story_items = story_reel['items']
 	
+def saveStory(story_items, newposts):
+	for i, media in enumerate(story_items):
+		if media['taken_at'] in newposts:
+			if media['media_type'] == 2:
+				story = media['video_versions'][0]['url']
+				urllib.request.urlretrieve(story, 'stories/story-%s.mp4' %i)
+			elif media['media_type'] == 1:
+				story = media['image_versions2']['candidates'][0]['url']
+				urllib.request.urlretrieve(story, 'stories/story-%s.jpg' %i)
+			else:
+				print('Error at saveStory')
+				sys.exit()
+		else:
+			continue
+			
+def getStoryFile():
+	story_files = []
+	for f in os.listdir('stories'):
+		story_files.append(f)
+	return story_files
+	
+	
+def identifyStoryType(story_files):
+	global containsVideo
+	containsVideo = False
+	for item in story_files:
+		if '.mp4' in item:
+			containsVideo = True
 
-consumer_key = os.environ.get('CONSUMER_KEY')
-consumer_secret = os.environ.get('CONSUMER_SECRET')
-access_token = os.environ.get('ACCESS_TOKEN')
-access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth)
-
-
-media_ids = []
-
-#if containsVideo:
-	#media_ids.append(media_files
-
-for i, media in enumerate(media_files):
-	media_ids.append(api.media_upload(media).media_id_string)
-
-
-if isAlbum and not containsVideo:
-	if len(media_ids) <= 4:
-		api.update_status(media_ids=media_ids, status='test')
-	elif len(media_ids) <= 8:
-		post1 = api.update_status(media_ids=media_ids[0:4], status='test')
-		post2 = api.update_status(media_ids=media_ids[4::], status='test2', in_reply_to_status_id=post1.id)
+def tweetStory(story_files, api):
+	identifyStoryType()
+	vid_ids = []
+	pic_ids = []
+	if containsVideo:
+		vid_ids.append(api.media_upload([vid for vid in story_files if '.mp4' in vid]))
+		pic_ids.append(api.media_upload([pic for pic in story_files if '.jpg' in vid]))
 	else:
-		print('more than 8 images')
-
-#jun 5 9:10pm MST - added image uploading + tweeting
-#work on formatting tweets next
-
+		pic_ids.append(api.media_upload([pic for pic in story_files if '.jpg' in vid]))
 
 	
-	
+	if len(vid_ids) == 0:
+		if len(pic_ids) <= 4:
+			api.update_status(media_ids=pic_ids, status='test')
+		elif len(pic_ids) <= 8:
+			story1 = api.update_status(media_ids=pic_ids[0:4], status='test')
+			story2 = api.update_status(media_ids=pic_ids[4::], status='test2', in_reply_to_status_id=story1.id)
+		elif len(pic_ids) <= 10:
+			story1 = api.update_status(media_ids=pic_ids[0:4], status='test')
+			story2 = api.update_status(media_ids=pic_ids[4:8], status='test2', in_reply_to_status_id=story1.id)
+			story3 = api.update_status(media_ids=pic_ids[8::], status='test3', in_reply_to_status_id=story2.id)
+		else:
+			print('Error at tweetStory. More than 10 story images')
+			
+	elif len(pic_ids) == 0:
+		story = api.update_status(media_ids=vid_ids[0], status='test')
+		vid_ids.pop(0)
+		for vid in vid_ids:
+			story = api.update_status(media_ids=vid, in_reply_to_status_id=story.id)
+	else:
+		if len(pic_ids) <= 4:
+			story = api.update_status(media_ids=pic_ids, status='test')
+			for vid in vid_ids:
+				story = api.update_status(media_ids=vid, in_reply_to_status_id=story.id)
+			
+		elif len(pic_ids) <= 8:
+			story = api.update_status(media_ids=pic_ids[0:4], status='test')
+			story = api.update_status(media_ids=pic_ids[4::], status='test2', in_reply_to_status_id=story.id)
+			for vid in vid_ids:
+				story = api.update_status(media_ids=vid, in_reply_to_status_id=story.id)
+		elif len(pic_ids) <= 10:
+			story = api.update_status(media_ids=pic_ids[0:4], status='test')
+			story = api.update_status(media_ids=pic_ids[4:8], status='test2', in_reply_to_status_id=story.id)
+			story = api.update_status(media_ids=pic_ids[8::], status='test3', in_reply_to_status_id=story.id)
+			for vid in vid_ids:
+				story = api.update_status(media_ids=vid, in_reply_to_status_id=story.id)
+		else:
+			print('Error at tweetStory. More than 10 story images with some videos')
+			
+			
+			
+with open ('storytimes.txt', 'a+') as storylogs:
+	newposts = []
+	for i, media in enumerate(story_items):
+		timestamp = media['taken_at']
+		storylogs.seek(0)
+		logs = storylogs.read();
+		if str(timestamp) not in logs:
+			newposts.append(timestamp)
+			storylogs.write(str(timestamp) + '\n')
+
+
+
+			
+if len(newposts) > 0:
+	saveStory(story_items, newposts)
+	story_files = getStoryFile()
+	identifyStoryType(story_files)
+	tweetStory(story_files, authTwitter())
+else:
+	print('No new story update')
+	sys.exit()
 
